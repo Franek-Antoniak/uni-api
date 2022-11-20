d31e4c2663c77a5d0f458daa1a1fc6a47d3a25b0
Franek Antoniak <franekant123@gmail.com>
2022-11-20 20:14:00 +0200

    Initial commit

---
+++ src/main/java/org/recru/task/UniApiApplication.java
@@ -0,0 +1,13 @@
+package org.recru.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/recru/task/UniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.recru.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
